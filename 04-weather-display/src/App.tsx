5456b56a38e149a1b49f9639036223beea3e70db
Andrew Davis <containerize.it@gmail.com>
2025-11-12 17:49:59 -0500

    some utils i will eventually use

--- 04-weather-display/src/App.tsx
+++ 04-weather-display/src/App.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import LocaleInput from "./components/LocaleInput";
-
+import monthsAndDays from "./lib/utils";
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({
+    data: [],
+    isLoading: false,
+    error: false,
+  });
+
   return (
     <section className="container">
       <LocaleInput />
---
+++ 04-weather-display/src/lib/utils.ts
@@ -0,0 +1,31 @@
+const monthsAndDays = () => {
+    const months: string[] = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    const days: string[] = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    const current = new Date();
+
+    return current;
+};
+
+export default monthsAndDays;
